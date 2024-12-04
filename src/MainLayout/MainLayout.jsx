dbf5a6a18b5b8e04c0c97f02f436b1827639af08
rasel3980 <rkrasel3980@gmail.com>
2024-12-04 21:59:44 +0600

    added reasonable and meaningful footer

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Components/Header';
+import Footer from '../Components/Footer';
 
 const MainLayout = () => {
     return (
@@ -9,6 +10,7 @@ const MainLayout = () => {
             <Header></Header>
             </div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
